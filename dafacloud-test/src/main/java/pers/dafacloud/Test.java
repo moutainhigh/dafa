d384ed6199f3fc572fd2418903c8a849908c1fb4
duke <2176311872@qq.com>
2019-04-29 20:08:23 +0800

    m

--- dafacloud-test/src/main/java/pers/dafacloud/Test.java
+++ dafacloud-test/src/main/java/pers/dafacloud/Test.java
@@ -7,8 +7,10 @@ public class Test {
    static String s  = System.getProperty("user.dir")+ File.separator+"template.txt";
 
     public static void main(String[] args) {
+        System.out.println("测试");
         System.out.println(System.getProperty("user.dir"));
         System.out.println(File.separator);
         System.out.println(s);
+        System.out.println("master");
     }
 }
