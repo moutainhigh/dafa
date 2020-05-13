f3b2e4230a7683d634d46d76d053f1df2aaf5a03
duke <2176311872@qq.com>
2020-05-13 21:46:12 +0800

    update .gitignore

--- dafagame-game/target/classes/pers/dafagame/cardLogics/brnn/CardSuit.java
+++
@@ -1,8 +0,0 @@
-package pers.dafagame.cardLogics.brnn;
-
-public enum CardSuit {
-    FANG_KUAI,
-    MEI_HUA,
-    HONG_TAO,
-    HEI_TAO;
-}
--- dafagame-game/target/classes/pers/dafagame/enums/CardSuit.java
+++
@@ -1,8 +0,0 @@
-package pers.dafagame.enums;
-
-/**
- * @Description:花色
- */
-public enum CardSuit {
-    FANG_KUAI, MEI_HUA, HONG_TAO, HEI_TAO;
-}
