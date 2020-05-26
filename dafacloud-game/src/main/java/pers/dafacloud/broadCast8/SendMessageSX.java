3f8e6ab50a7db9a4fc041d520bb7de7069d60bef
duke <2176311872@qq.com>
2020-05-26 18:09:07 +0800

    m

--- dafacloud-game/src/main/java/pers/dafacloud/broadCast8/SendMessageSX.java
+++ dafacloud-game/src/main/java/pers/dafacloud/broadCast8/SendMessageSX.java
@@ -7,12 +7,13 @@ import java.net.URI;
 import java.util.UUID;
 
 public class SendMessageSX {
+    static WebSocketContainer container = ContainerProvider.getWebSocketContainer();
 
     public static void process() {
         try {
-            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+
             ResponceMessage responceMessage = new ResponceMessage();//返回信息类
-            Session session = container.connectToServer(responceMessage, URI.create("ws://dafacloud-test.com/v1/broadCast/security?type=8&uuid="+UUID.randomUUID()+"_"+System.currentTimeMillis()));
+            Session session = container.connectToServer(responceMessage, URI.create("ws://dafacloud-test.com/v1/broadCast/security?type=8&uuid=" + UUID.randomUUID() + "_" + System.currentTimeMillis()));
             session.setMaxIdleTimeout(5000);
             //session.setMaxTextMessageBufferSize(2048000);//设置缓冲文本大小
             session.setMaxTextMessageBufferSize(128);//设置缓冲文本大小
